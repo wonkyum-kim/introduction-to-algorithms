3bea15cdbba90e7473d07bf90dd9b1c769f9943b
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-07-25 12:11:14 +0900

    Add files via upload

---
+++ chapter 2/2.3.7 two sum.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+template<typename T>
+bool two_sum(std::vector<T>& S, const T& key)
+{
+	size_t left = 0;
+	size_t right = S.size()-1;
+	std::sort(S.begin(), S.end());
+
+	while (left < right)
+	{
+		if ((S[left] + S[right]) < key)
+			left += 1;
+		else if ((S[left] + S[right]) > key)
+			right -= 1;
+		else
+			true;
+	}
+
+	return false;
+}
+
+int main()
+{
+	std::vector<int> S{ 5,2,4,3,1 };
+	two_sum(S, 5);
+}
