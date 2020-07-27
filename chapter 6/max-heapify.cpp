47f5fc41444cea050cd666f4032620608ac0b4ea
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-07-27 15:06:23 +0900

    Create max-heapify.cpp

---
+++ chapter 6/max-heapify.cpp
@@ -0,0 +1,19 @@
+size_t left(size_t& l)
+{
+return 2l;
+}
+
+size_t right (size_t& r)
+{
+return 2r+1;
+}
+
+template <typename T>
+void max_heapify(std::vector<T>& A, size_t i)
+{
+size_t l = left(i);
+size_t r = right(i);
+
+if(l <=A.)
+
+}
