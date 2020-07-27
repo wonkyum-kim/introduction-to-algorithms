a5632736b882ea7a7209cb2b7874bb60a2eaf2f3
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-07-27 21:16:54 +0900

    Update max-heapify.cpp

--- chapter 6/max-heapify.cpp
+++ chapter 6/max-heapify.cpp
@@ -1,19 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
 size_t left(size_t& l)
 {
-return 2l;
+	return 2*l;
 }
 
-size_t right (size_t& r)
+size_t right(size_t& r)
 {
-return 2r+1;
+	return 2*r + 1;
 }
 
 template <typename T>
 void max_heapify(std::vector<T>& A, size_t i)
 {
-size_t l = left(i);
-size_t r = right(i);
+	size_t l = left(i);
+	size_t r = right(i);
+	size_t largest;
 
-if(l <=A.)
+	if (l <= A.size() && A[l] > A[i])
+		largest = l;
+	else
+		largest = i;
 
+	if (r <= A.size() && A[r] > A[i])
+		largest = r;
+	else
+		largest = i;
+
+	if (largest != i)
+	{
+		std::swap(A[i], A[largest]);
+		max_heapify(A, largest);
+	}
 }
