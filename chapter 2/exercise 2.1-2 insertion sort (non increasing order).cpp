a583f4819365ce7b5427b35b117e3a63d75cfdf6
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-07-29 18:18:15 +0900

    Update exercise 2.1-2 insertion sort (non increasing order).cpp

--- chapter 2/exercise 2.1-2 insertion sort (non increasing order).cpp
+++ chapter 2/exercise 2.1-2 insertion sort (non increasing order).cpp
@@ -1,24 +1,26 @@
-#include <cassert>
 #include <vector>
 #include <iostream>
 
-template <typename T, typename Comp = std::less<T>>
-void insertionSort(std::vector<T>& A, Comp comp = Comp()) {
-    for (size_t j = 1; j < A.size(); j++) {
+template <typename T>
+void insertion_sort(std::vector<T>& A)
+{
+    for (auto j = 1; j < A.size(); ++j)
+    {
         T key = A[j];
-        size_t i = j - 1;
-        while (i < A.size() && comp(A[i], key)) {
-            A[i+1] = A[i];
+        auto i = j - 1;
+        while (i>=0 && A[i] < key)
+        {
+            A[i + 1] = A[i];
             i--;
         }
         A[i + 1] = key;
     }
 }
 
-int main() {
+int main()
+{
     std::vector<int> v{ 5, 2, 3, 1, 4 };
-    insertionSort(v);
-    for (auto n : v) {
+    insertion_sort(v);
+    for (auto n : v)
         std::cout << n << ' ';
-    }
 }
