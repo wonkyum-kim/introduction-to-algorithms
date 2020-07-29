8a6ef331b02c655654f25d649ca5e520fbdae417
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-07-29 09:06:08 +0900

    Update exercise 2.1-2 insertion sort (increasing order).cpp

--- chapter 2/exercise 2.1-2 insertion sort (increasing order).cpp
+++ chapter 2/exercise 2.1-2 insertion sort (increasing order).cpp
@@ -1,14 +1,14 @@
 #include <vector>
 #include <iostream>
 
-template <typename T, typename Comp = std::greater<T>>
-void insertion-sort(std::vector<T>& A, Comp comp = Comp()) 
+template <typename T>
+void insertion-sort(std::vector<T>& A) 
 {
     for (size_t j = 1; j < A.size(); ++j) 
     {
         T key = A[j];
         size_t i = j - 1;
-        while (i < A.size() && comp(A[i], key)) 
+        while (A[i] > key) 
         {
             A[i + 1] = A[i];
             i--;
