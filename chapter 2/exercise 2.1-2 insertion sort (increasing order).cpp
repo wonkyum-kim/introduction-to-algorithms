35a7ab5176ed0894ec7abf0de58ddca04005bf9a
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-07-29 18:17:28 +0900

    Update exercise 2.1-2 insertion sort (increasing order).cpp

--- chapter 2/exercise 2.1-2 insertion sort (increasing order).cpp
+++ chapter 2/exercise 2.1-2 insertion sort (increasing order).cpp
@@ -2,31 +2,25 @@
 #include <iostream>
 
 template <typename T>
-void insertion-sort(std::vector<T>& A) 
+void insertion_sort(std::vector<T>& A)
 {
-    for (size_t j = 1; j < A.size(); ++j) 
+    for (auto j = 1; j < A.size(); ++j)
     {
         T key = A[j];
-        size_t i = j - 1;
-        while (A[i] > key) 
+        auto i = j - 1;
+        while (i>=0 && A[i] > key)
         {
             A[i + 1] = A[i];
-            if (i > 0)
-                i--;
-            else
-                break;
+            i--;
         }
-        if (i = 0)
-            A[i] = key;
-        else
-            A[i + 1] = key;
+        A[i + 1] = key;
     }
 }
 
-int main() 
+int main()
 {
     std::vector<int> v{ 5, 2, 3, 1, 4 };
     insertion_sort(v);
-    for (auto n : v) 
+    for (auto n : v)
         std::cout << n << ' ';
 }
