9195ee93206618f37b47a1407df3270f724b36ed
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-07-29 21:13:52 +0900

    Update heap.cpp

--- chapter 6/heap.cpp
+++ chapter 6/heap.cpp
@@ -1,62 +1,67 @@
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <utility>
+#include<iostream>
+#include<vector>
 
-// index of root node is 1. 
-size_t parent(const size_t& i)
+int parent(int i)
 {
-	return i/2;
+    return (i - 1) / 2;
 }
 
-size_t left(const size_t& i)
+int left(int i)
 {
-	return 2*i;
+    return 2 * i + 1;
 }
 
-size_t right(const size_t& i)
+int right(int i)
 {
-	return 2*i + 1;
-}
-
-template <typename T>
-void max_heapify(std::pair<std::vector<T>&, size_t>& B, size_t i)
-{
-	size_t l = left(i);
-	size_t r = right(i);
-	size_t largest = i;
-
-	if (l <= B.second && B.first[l] > B.first[i])
-		largest = l;
-
-	if (r <= B.second && B.first[r] > B.first[i])
-		largest = r;
-
-	if (largest != i)
-	{
-		std::swap(B.first[i], B.first[largest]);
-		max_heapify(B, largest);
-	}
+    return 2 * i + 2;
 }
 
 template<typename T>
-void build_max_heap(std::vector<T>& A)
+void max_heapify(std::vector<T>& A, int n, int i)
 {
-        size_t A_heap_size = A.size()-1;
-        auto B = std::make_pair(A, A_heap_size);
-        // terminal nodes are start at A_heap_size / 2 + 1
-	for (size_t i = A_heap_size / 2; i >= 1; --i)
-		max_heapify(B, i);
+    int l = left(i);
+    int r = right(i);
+    int largest = i;
+
+    if (l <= (n - 1) && A[l] > A[i])
+        largest = l;
+ 
+    if (r <= (n - 1) && A[r] > A[largest])
+        largest = r;
+
+    if (largest != i)
+    {
+        std::swap(A[i], A[largest]);
+        max_heapify(A, n, largest);
+    }
 }
 
 template<typename T>
-void heap_sort(std::pair<std::vector<T>&, size_t>& A)
+void build_max_Heap(std::vector<T>& A, int n)
 {
-	build_max_heap(A.first);
-	for(size_t i = A.first.size() - 1; i >=2; --i)
-	{
-	    std::swap(A.first[1], A.first[i]);
-	    A.second--;
-            max_heapify(A, 1);
-        }
+    for (int i = n / 2 - 1; i >= 0; i--)
+        max_heapify(A, n, i);
+
+}
+
+template<typename T>
+void heap_sort(std::vector<T>& A, int n)
+{
+    build_max_Heap(A, n);
+    for (int i = n - 1; i > 0; i--)
+    {
+        std::swap(A[0], A[i]);
+        max_heapify(A, --n, 0);
+    }
+}
+
+
+int main()
+{
+    std::vector<int> A {9,6,3,8,5,2,7,4,1,0};
+    heap_sort(A, A.size());
+    for (int i = 0; i < A.size(); i++)
+        std::cout << A[i] << " ";
+   
+    return 0;
 }
