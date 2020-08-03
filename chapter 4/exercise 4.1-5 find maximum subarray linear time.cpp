d3e8ec795ac7be10f5744fb01f69bd9588367e09
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-03 20:20:19 +0900

    Create exercise 4.1-5 find maximum subarray linear time.cpp

---
+++ chapter 4/exercise 4.1-5 find maximum subarray linear time.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <tuple>
+
+using ans_type = std::tuple<size_t, size_t, int>;
+
+template<typename T>
+ans_type find_maximum_subarray_linear(const std::vector<T>& A)
+{
+	int max_sum = std::numeric_limits<int>::min();		// sentinel value
+	int running sum = 0;
+	size_t max_left = 0;
+	size_t max_right = 0;
+	size_t curr_left = 0;
+	size_t curr_right = 0;
+
+	for (size_t i = 0; i < A.size(); i++)
+	{
+		if (running sum <= 0)
+		{
+			curr_left = i;
+			curr_right = i;
+			running_sum = A[i];
+		}
+		else
+		{
+			running_sum += A[i];
+			curr_right = i;
+		}
+
+		if (running_sum > max_sum)
+		{
+			max_sum = running_sum;
+			max_left = curr_left;
+			max_right = curr_right;
+		}
+	}
+	return { max_left, max_right, max_sum };
+}
