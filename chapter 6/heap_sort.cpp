50ae139fa6c6ec7210290b7183d766f02c1df820
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-04 18:39:53 +0900

    Update heap_sort.cpp

--- chapter 6/heap_sort.cpp
+++ chapter 6/heap_sort.cpp
@@ -1,6 +1,3 @@
-
-// start !
-
 #include<iostream>
 #include<vector>
 #include<algorithm>
@@ -38,7 +35,7 @@ void max_heapify(std::vector<T>& A, int h_size, size_t i)
     if(largest != i)
     {
         std::swap(A[i], A[largest]);
-        mx_hepify(A,h_size,largest);
+        max_heapify(A,h_size,largest);
     }
 }
 
@@ -52,81 +49,19 @@ void build_max_heap(std::vector<T>& A, int h_size)
 template<typename T>
 void heap_sort(std::vector<T>& A, int h_size)
 {
-    buld_max_heap(A, h_size);
+    build_max_heap(A, h_size);
     for(size_t i = h_size; i > 1; --i)
     {
         std::swap(A[1], A[i]);
-        h_size -= 1;
-        max_heapify(A,1);
+        max_heapify(A,--h_size,1);
     }
 }
-
-
-//*********************************************************************************************
-
-#include<iostream>
-#include<vector>
-
-int parent(int i)
-{
-    return (i - 1) / 2;
-}
-
-int left(int i)
-{
-    return 2 * i + 1;
-}
-
-int right(int i)
-{
-    return 2 * i + 2;
-}
-
-template<typename T>
-void max_heapify(std::vector<T>& A, int n, int i)
-{
-    int l = left(i);
-    int r = right(i);
-    int largest = i;
-
-    if (l <= (n - 1) && A[l] > A[i])
-        largest = l;
- 
-    if (r <= (n - 1) && A[r] > A[largest])
-        largest = r;
-
-    if (largest != i)
-    {
-        std::swap(A[i], A[largest]);
-        max_heapify(A, n, largest);
-    }
-}
-
-template<typename T>
-void build_max_Heap(std::vector<T>& A, int n)
-{
-    for (int i = n / 2 - 1; i >= 0; i--)
-        max_heapify(A, n, i);
-
-}
-
-template<typename T>
-void heap_sort(std::vector<T>& A, int n)
-{
-    build_max_Heap(A, n);
-    for (int i = n - 1; i > 0; i--)
-    {
-        std::swap(A[0], A[i]);
-        max_heapify(A, --n, 0);
-    }
-}
-
 
 int main()
 {
-    std::vector<int> A {9,6,3,8,5,2,7,4,1,0};
-    heap_sort(A, A.size());
-    for (int i = 0; i < A.size(); i++)
+    std::vector<int> A {0,9,6,3,8,5,2,7,4,1,0};
+    heap_sort(A, A.size() - 1);     // A.size() - 1 is h_size
+    for (int i = 1; i < A.size(); i++)
         std::cout << A[i] << " ";
     return 0;
 }
