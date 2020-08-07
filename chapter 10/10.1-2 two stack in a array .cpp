7eeace64ebeb20c6adb554f9bff4e0d94543c903
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-07 10:23:50 +0900

    Create 10.1-2 two stack in a array .cpp

---
+++ chapter 10/10.1-2 two stack in a array .cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+#include<array>
+#include<stdexcept>
+
+template<typename T, size_t N>
+struct Two_Stack {
+    std::array<T, N> data;
+    size_t left;
+    size_t right;
+    
+    Two_Stack(std::array<T, N> d, size_t l, size_t r) : data{d}, left{l}, right{r}  {};
+    void push_l(const T x);
+    void push_r(const T x);
+    T pop_l();
+    T pop_r();
+};
+
+template<typename T, size_t N>
+void Two_Stack<T, N>::push_l(const T x)
+{
+    if(right < left)
+        throw std::overflow_error("Stack overflow");
+    else
+        data[left++] = x;
+}
+
+template<typename T, size_t N>
+void Two_Stack<T, N>::push_r(const T x)
+{
+    if(right < left)
+        throw std::overflow_error("Stack overflow");
+    else
+        data[right--] = x;
+}
+
+template<typename T, size_t N>
+T Two_Stack<T, N>::pop_l()
+{
+    if(left == 0)
+        return data[0];
+    else
+        return data[--left];
+}
+
+template<typename T, size_t N>
+T Two_Stack<T, N>::pop_r()
+{
+    if(right == data.size() - 1)
+        return data[data.size() - 1];
+    else
+        return data[++right];
+}
+
+int main()
+{
+    std::array<int, 4> data;
+    size_t left = 0;
+    size_t right = data.size() - 1;     // 3
+    Two_Stack ts {data, left, right};
+    ts.push_l(1);
+    ts.push_l(2);
+    ts.push_r(4);
+    ts.push_r(3);
+
+    std::cout << ts.pop_l() << ' ' << ts.pop_l() << ' ' << ts.pop_r() << ' ' << ts.pop_r();     // 2 1 3 4
+}
