868a8215e41c50541d78119c49088e8b1fa96c28
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-09 22:59:55 +0900

    deque

--- chapter 10/10.1-5 double ended queue.cpp
+++ chapter 10/10.1-5 double ended queue.cpp
@@ -3,12 +3,12 @@
 #include<stdexcept>
 
 template<typename T, size_t N>
-struct Double_ended_queue{
+struct Double_ended_queue {
     std::array<T, N> data;
     int head = 0;
     int tail = 0;
-    Double_ended_queue(std::array<T, N> d, int h, int t) : data{d}, head{h}, tail{t}   {};
-    
+    Double_ended_queue(int h, int t) : data{}, head{ h }, tail{ t }   {};
+
     void head_enqueue(const T x);
     void tail_enqueue(const T x);
     T head_dequeue();
@@ -18,45 +18,33 @@ struct Double_ended_queue{
 template<typename T, size_t N>
 void Double_ended_queue<T, N>::head_enqueue(const T x)
 {
-    // array is full
-    if(head == tail + 1 || (head == 0 && tail == data.size()))
-        throw std::overflow_error("Queue overflow");
+    if (head == 0)
+        head == data.size() - 1;
     else
-    {
-        if(head == 0)        
-            head == data.size() - 1;
-        else
-            head --;
-        data[head] = x;
-    }
+        head--;
+    data[head] = x;
 }
 
 template<typename T, size_t N>
 void Double_ended_queue<T, N>::tail_enqueue(const T x)
 {
-    // array is full
-    if(head == tail + 1 || (head == 0 && tail == data.size()))
-        throw std::overflow_error("Queue overflow");
+    data[tail] = x;
+    if (tail == data.size() - 1)
+        tail = 0;
     else
-    {
-        data[tail] = x;
-        if(tail == data.size() - 1)     
-            tail = 0;
-        else
-            tail++;
-    }
+        tail++;
 }
 
 template<typename T, size_t N>
 T Double_ended_queue<T, N>::head_dequeue()
 {
-    // array is empty
-    if(head == tail)
+    // the array is empty
+    if (head == tail)
         throw std::underflow_error("Queue underflow");
     else
     {
         T x = data[head];
-        if(head == data.size() - 1)
+        if (head == data.size() - 1)
             head = 0;
         else
             head++;
@@ -67,12 +55,12 @@ T Double_ended_queue<T, N>::head_dequeue()
 template<typename T, size_t N>
 T Double_ended_queue<T, N>::tail_dequeue()
 {
-    // array is empty
-    if(head == tail)
+    // the array is empty
+    if (head == tail)
         throw std::underflow_error("Queue underflow");
     else
     {
-        if(tail == 0)
+        if (tail == 0)
             tail = data.size() - 1;
         else
             tail--;
@@ -84,13 +72,13 @@ T Double_ended_queue<T, N>::tail_dequeue()
 int main()
 {
     std::array<int, 4> data;
-    int head = 1;
-    int tail = 1;
-    Double_ended_queue dq {data, head, tail};
-    
+    int head = 0;
+    int tail = 0;
+    Double_ended_queue<int, 50> dq{head, tail };
+
     dq.tail_enqueue(1);
     dq.tail_enqueue(2);
     dq.tail_enqueue(3);
-    
+
     std::cout << dq.tail_dequeue() << dq.tail_dequeue() << dq.tail_dequeue();
 }
