bfef9f787d4026f55eab7032aa0d16aaae3baf50
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-20 22:53:12 +0900

    chapter 2

--- chapter 2/exercise 2.3.7 two sum.cpp
+++ chapter 2/exercise 2.3.7 two sum.cpp
@@ -9,21 +9,22 @@ bool two_sum(std::vector<T>& S, const T& key)
 	size_t right = S.size()-1;
 	std::sort(S.begin(), S.end());
 
-	while (left < right)
-	{
+	while (left < right) {
 		if ((S[left] + S[right]) < key)
-			left += 1;
+			left++;
 		else if ((S[left] + S[right]) > key)
-			right -= 1;
+			right--;
 		else
 			return true;
 	}
-
 	return false;
 }
 
 int main()
 {
 	std::vector<int> S{ 5,2,4,3,1 };
-	two_sum(S, 5);
+	if(two_sum(S, 5))
+	    std::cout << "true";
+	else
+	    std::cout << "false";
 }
