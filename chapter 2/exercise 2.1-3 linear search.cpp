0179c9d53bc1f350b6c542f85ec287cdbed77900
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-20 21:05:20 +0900

    chapter 2

--- chapter 2/exercise 2.1-3 linear search.cpp
+++ chapter 2/exercise 2.1-3 linear search.cpp
@@ -2,11 +2,10 @@
 #include <iostream>
 
 template<typename T>
-int linear_search(std::vector<T>& A, const T& key)
+int linear_search(std::vector<T>& A, const T key)
 {
-	for (size_t i = 0; i<A.size(); ++i)
-	{
-		if (key==A[i])
+	for (size_t i = 0; i < A.size(); ++i) {
+		if (key == A[i])
 			return i;
 	}
 	return -1;
@@ -16,5 +15,4 @@ int main()
 {
 	std::vector<int> v = { 5,2,3,1,4 };
 	std::cout << linear_search(v, 3) + 1;
-	return 0;
 }
