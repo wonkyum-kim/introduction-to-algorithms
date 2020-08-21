6ff4ee41f1d181a035e22faa8e89a2ad54dcef6f
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-21 21:20:58 +0900

    chapter 2

--- chapter 2/merge sort with sentinel.cpp
+++ chapter 2/merge sort with sentinel.cpp
@@ -6,35 +6,39 @@
 template<typename T>
 void merge(std::vector<T>& A, const size_t p, const size_t q, const size_t r)
 {
-    size_t n1 = q - p + 1;  // L.size() 
-    size_t n2 = r - q;      // R.size() 
+    int n1 = q - p + 1;
+    int n2 = r - q;
+
     std::vector<T> L;
     std::vector<T> R;
+    L.push_back(0);
+    R.push_back(0);
 
-    for(auto i = 0; i < n1; ++i)
-        L.push_back(A[p + i]);
-    for(auto j = 0; j < n2; ++j)
-        R.push_back(A[q + j + 1]);
-        
-    L.push_back(std::numeric_limits<T>::max());     // sentinel
-    R.push_back(std::numeric_limits<T>::max());     // sentinel
+    for (auto i = 1; i <= n1; ++i) 
+        L.push_back(A[p + i - 1]);
+    for (auto j = 1; j <= n2; ++j)
+        R.push_back(A[q + j]);
+
+    L.push_back(std::numeric_limits<int>::max());
+    R.push_back(std::numeric_limits<int>::max());
     
-    size_t i = 0;
-    size_t j = 0;
-    
-    for(auto k = p; k <= r; ++k) {
-        if(L[i] <= R[j])
+    size_t i = 1;
+    size_t j = 1;
+
+    for (auto k = p; k <= r; ++k) {
+        if (L[i] <= R[j])
             A[k] = L[i++];
         else
             A[k] = R[j++];
     }
+
 }
 
 // devide and conquer
 template<typename T>
 void merge_sort(std::vector<T>& A, const size_t p, const size_t r)
 {
-    if(p < r) {
+    if (p < r) {
         size_t q = (p + r) / 2;
         merge_sort(A, p, q);
         merge_sort(A, q + 1, r);
@@ -44,8 +48,8 @@ void merge_sort(std::vector<T>& A, const size_t p, const size_t r)
 
 int main()
 {
-    std::vector<int> A{ 5,2,4,1,3 };
-	merge_sort(A, 0, A.size() - 1);
-	for (auto& x : A)
-		std::cout << x << ' ';
+    std::vector<int> A{ 2, 1, 3, 5, 4 };
+    merge_sort(A, 0, A.size() - 1);
+    for (auto& x : A)
+        std::cout << x << ' ';
 }
