1cad34fe9b0ff241a220b4780b1f2581b493c86a
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-23 16:55:53 +0900

    chapter 4

--- chapter 4/exercise 4.1-5 find maximum subarray linear time.cpp
+++ chapter 4/exercise 4.1-5 find maximum subarray linear time.cpp
@@ -15,26 +15,30 @@ ans_type find_maximum_subarray_linear(const std::vector<T>& A)
 	size_t curr_left = 0;
 	size_t curr_right = 0;
 
-	for (size_t i = 0; i < A.size(); i++)
-	{
-		if (running_sum <= 0)
-		{
+	for (size_t i = 1; i < A.size(); i++) {
+		if (running_sum <= 0) {
 			curr_left = i;
 			curr_right = i;
 			running_sum = A[i];
 		}
-		else
-		{
+		else {
 			running_sum += A[i];
 			curr_right = i;
 		}
 
-		if (running_sum > max_sum)
-		{
+		if (running_sum > max_sum) {
 			max_sum = running_sum;
 			max_left = curr_left;
 			max_right = curr_right;
 		}
 	}
 	return { max_left, max_right, max_sum };
+}
+
+int main()
+{
+	// A[0] is sentinel value.
+	std::vector<int> A{ 0, 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+	auto arr = find_maximum_subarray_linear(A);
+	std::cout << std::get<0>(arr) << ' ' << std::get<1>(arr) << ' ' << std::get<2>(arr);
 }
