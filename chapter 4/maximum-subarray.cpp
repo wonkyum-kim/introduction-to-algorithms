d1a9d05f6ed1e23e5e617ca335f9f4256b0f07a0
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-23 16:44:16 +0900

    chapter 4

--- chapter 4/maximum-subarray.cpp
+++ chapter 4/maximum-subarray.cpp
@@ -6,32 +6,27 @@
 using ans_type = std::tuple<size_t, size_t, int>;
 
 template<typename T>
-ans_type find_max_crossing_subarray(const std::vector<T>& A, size_t low, size_t mid, size_t high)
+ans_type find_max_crossing_subarray(const std::vector<T>& A, const size_t low, const size_t mid, const size_t high)
 {
 	int left_sum = std::numeric_limits<int>::min();		// sentinel value
 	int sum = 0;
 	size_t max_left = 0;
 
-	for (size_t i = mid; i >= low; --i)
-	{
+	for (size_t i = mid; i >= low; --i) {
 		sum += A[i];
-		if (sum > left_sum)
-		{
+		if (sum > left_sum) {
 			left_sum = sum;
 			max_left = i;
 		}
 	}
 
 	int right_sum = std::numeric_limits<int>::min();	// sentinel value
-	int sum = 0;
+	sum = 0;
 	size_t max_right = 0;
 
-
-	for (size_t i = mid + 1; i <= high; ++i)
-	{
+	for (size_t i = mid + 1; i <= high; ++i) {
 		sum += A[i];
-		if (sum > right_sum)
-		{
+		if (sum > right_sum) {
 			right_sum = sum;
 			max_right = i;
 		}
@@ -40,12 +35,11 @@ ans_type find_max_crossing_subarray(const std::vector<T>& A, size_t low, size_t 
 }
 
 template <typename T>
-ans_type find_maximum_subarray(const std::vector<T>& A, size_t low, size_t high)
+ans_type find_maximum_subarray(const std::vector<T>& A, const size_t low, const size_t high)
 {
 	if (high == low)	// only one element
 		return { low, high, A[low] };
-	else
-	{
+	else {
 		size_t mid = (low + high) / 2;
 		auto [l_low, l_high, l_sum] = find_maximum_subarray(A, low, mid);
 		auto [r_low, r_high, r_sum] = find_maximum_subarray(A, mid + 1, high);
@@ -58,4 +52,13 @@ ans_type find_maximum_subarray(const std::vector<T>& A, size_t low, size_t high)
 		else
 			return { c_low, c_high, c_sum };
 	}
+}
+
+int main()
+{
+	std::vector<int> A{ 0, 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+	size_t low = 1;
+	size_t high = 16;
+	auto arr = find_maximum_subarray(A, low, high);
+	std::cout << std::get<0>(arr) << ' ' << std::get<1>(arr) << ' ' << std::get<2>(arr);
 }
