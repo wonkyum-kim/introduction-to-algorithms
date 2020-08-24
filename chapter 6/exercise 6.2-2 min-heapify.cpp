e88f38e70a2131adc0909c73828ad5de5162db92
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-24 21:20:18 +0900

    chapter 6

--- chapter 6/exercise 6.2-2 min-heapify.cpp
+++ chapter 6/exercise 6.2-2 min-heapify.cpp
@@ -3,23 +3,22 @@
 #include <vector>
 
 template<typename T>
-void max_heapify(std::vector<T>& A, int h_size, size_t i)
+void min_heapify(std::vector<T>& A, const int h_size, const size_t i)
 {
     size_t l = left(i);
     size_t r = right(i);
     size_t smallest = i;
-    
-    if(l <= h_size && A[l] < A[i])
+
+    if (l <= h_size && A[l] < A[i])
         smallest = l;
-    
-    if(r <= h_size && A[r] < A[smallest])
+
+    if (r <= h_size && A[r] < A[smallest])
         smallest = r;
-        
-    if(largest != i)
+
+    if (largest != i)
     {
         std::swap(A[i], A[smallest]);
         max_heapify(A, h_size, smallest);
     }
 }
-
 
