ed53e856144dc81b947615735eed2e062637ba95
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-24 21:54:22 +0900

    chapter 6

--- chapter 6/priority queue.cpp
+++ chapter 6/priority queue.cpp
@@ -26,24 +26,24 @@ void max_heapify(std::vector<T>& A, int h_size, size_t i)
     size_t l = left(i);
     size_t r = right(i);
     size_t largest = i;
-    
-    if(l <= h_size && A[l] > A[i])
+
+    if (l <= h_size && A[l] > A[i])
         largest = l;
-    
-    if(r <= h_size && A[r] > A[largest])
+
+    if (r <= h_size && A[r] > A[largest])
         largest = r;
-        
-    if(largest != i)
+
+    if (largest != i)
     {
         std::swap(A[i], A[largest]);
-        max_heapify(A,h_size,largest);
+        max_heapify(A, h_size, largest);
     }
 }
 
 template<typename T>
 void build_max_heap(std::vector<T>& A, int h_size)
 {
-    for(size_t i = h_size / 2; i > 0; --i)
+    for (size_t i = h_size / 2; i > 0; --i)
         max_heapify(A, h_size, i);
 }
 
@@ -51,15 +51,15 @@ template<typename T>
 void heap_sort(std::vector<T>& A, int h_size)
 {
     build_max_heap(A, h_size);
-    for(size_t i = h_size; i > 1; --i)
+    for (size_t i = h_size; i > 1; --i)
     {
         std::swap(A[1], A[i]);
-        max_heapify(A,--h_size,1);
+        max_heapify(A, --h_size, 1);
     }
 }
 
 template<typename T>
-T heap_maximum(std::vector<T>& A)
+T heap_maximum(const std::vector<T>& A)
 {
     return A[1];
 }
@@ -67,21 +67,21 @@ T heap_maximum(std::vector<T>& A)
 template<typename T>
 T heap_extract_max(std::vector<T>& A, int h_size)
 {
-    if(h_size < 1)
+    if (h_size < 1)
         throw std::underflow_error("heap underflow");
     T max = A[1];
-    A[1] = A[h_size - 1];
-    max_heapify(A, h_size - 1, 1);
+    A[1] = A[h_size];
+    max_heapify(A, --h_size, 1);
     return max;
 }
 
 template<typename T>
-void heap_increase_key(std::vector<T>& A, size_t i, T key)
+void heap_increase_key(std::vector<T>& A, size_t i, const T key)
 {
-    if(key < A[i])
+    if (key < A[i])
         throw std::runtime_error("new key is smaller than current key");
     A[i] = key;
-    while(i > 1 && A[parent(i)] < A[i])
+    while (i > 1 && A[parent(i)] < A[i])
     {
         std::swap(A[i], A[parent(i)]);
         i = parent(i);
@@ -99,10 +99,10 @@ void max_heap_insert(std::vector<T>& A, int h_size, T key)
 
 int main()
 {
-    std::vector<int> A {0,9,6,3,8,5,2,7,4,1,0};
+    std::vector<int> A{ 0,9,6,3,8,5,2,7,4,1,0 };
     std::make_heap(A.begin() + 1, A.end());
     max_heap_insert(A, A.size() - 1, 6);
-    for(auto i = 1; i < A.size() - 1; ++i)
+    for (auto i = 1; i < A.size() - 1; ++i)
         std::cout << A[i] << ' ';
     return 0;
 }
