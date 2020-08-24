b5fae915e6b99b529d8db51a49d0b427271f0f57
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-08-24 21:18:08 +0900

    chapter 6

--- chapter 6/heap_sort.cpp
+++ chapter 6/heap_sort.cpp
@@ -20,29 +20,29 @@ int right(int i)
 
 
 template<typename T>
-void max_heapify(std::vector<T>& A, int h_size, size_t i)
+void max_heapify(std::vector<T>& A, const int h_size, const size_t i)
 {
     size_t l = left(i);
     size_t r = right(i);
     size_t largest = i;
-    
-    if(l <= h_size && A[l] > A[i])
+
+    if (l <= h_size && A[l] > A[i])
         largest = l;
-    
-    if(r <= h_size && A[r] > A[largest])
+
+    if (r <= h_size && A[r] > A[largest])
         largest = r;
-        
-    if(largest != i)
+
+    if (largest != i)
     {
         std::swap(A[i], A[largest]);
-        max_heapify(A,h_size,largest);
+        max_heapify(A, h_size, largest);
     }
 }
 
 template<typename T>
-void build_max_heap(std::vector<T>& A, int h_size)
+void build_max_heap(std::vector<T>& A, const int h_size)
 {
-    for(size_t i = h_size / 2; i > 0; --i)
+    for (size_t i = h_size / 2; i > 0; --i)
         max_heapify(A, h_size, i);
 }
 
@@ -50,16 +50,17 @@ template<typename T>
 void heap_sort(std::vector<T>& A, int h_size)
 {
     build_max_heap(A, h_size);
-    for(size_t i = h_size; i > 1; --i)
+    for (size_t i = h_size; i > 1; --i)
     {
         std::swap(A[1], A[i]);
-        max_heapify(A,--h_size,1);
+        max_heapify(A, --h_size, 1);
     }
 }
 
 int main()
 {
-    std::vector<int> A {0,9,6,3,8,5,2,7,4,1,0};
+    constexpr int dummy = 0;
+    std::vector<int> A{ dummy,9,6,3,8,5,2,7,4,1,0 };
     heap_sort(A, A.size() - 1);     // A.size() - 1 is h_size
     for (int i = 1; i < A.size(); i++)
         std::cout << A[i] << " ";
