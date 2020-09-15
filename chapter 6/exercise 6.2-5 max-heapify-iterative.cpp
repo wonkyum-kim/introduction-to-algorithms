4d03582598e9f55078fca1929e93e57456d508bf
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-09-15 21:30:22 +0900

    chapter 6

--- chapter 6/exercise 6.2-5 max-heapify-iterative.cpp
+++ chapter 6/exercise 6.2-5 max-heapify-iterative.cpp
@@ -34,7 +34,7 @@ void max_heapify_iterative(std::vector<T>& A, int h_size)
             largest = r;
     }
     
-    if(largest != )
+    if(largest != i)
     {
         std::swap(A[largest], A[i]);
         i = largest;
