e2866859aaab0a2cc4ecd9edf61f532027c4dcfd
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-10-29 13:10:44 +0900

    chapter 10

--- chapter 10/10.1-6 queue using two stacks.cpp
+++ chapter 10/10.1-6 queue using two stacks.cpp
@@ -1,45 +1,47 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <numeric>
+#include <cassert>
+#include <queue>
 #include <stack>
 
 template<typename T>
 struct Queue {
 	std::stack<T> A;
 	std::stack<T> B;
-	
 	Queue() : A{}, B{}	{};
-	void enqueue(const T x);
-	T dequeue();
+	void push(const T x);
+	T pop();
 };
 
 template<typename T>
-void Queue<T>::enqueue(const T x)
+void Queue<T>::push(const T x) 
 {
 	A.push(x);
 }
 
 template<typename T>
-T Queue<T>::dequeue()
+T Queue<T>::pop()
 {
-	if (B.empty())
-	{
-		while (!A.empty())
-		{
+	if (B.empty()) {
+		while (!A.empty()) {
 			T x = A.top();
 			A.pop();
 			B.push(x);
 		}
 	}
-	T x = B.top();
+	T y = B.top();
 	B.pop();
-	return x;
+	return y;
 }
 
 int main()
 {
 	Queue<int> q;
-	q.enqueue(1);
-	q.enqueue(2);
-	q.enqueue(3);
-	q.enqueue(4);
-	std::cout << q.dequeue() << q.dequeue() << q.dequeue() << q.dequeue();
+	q.push(1);
+	q.push(2);
+	q.push(3);
+	q.push(4);
+	std::cout << q.pop() << q.pop() << q.pop() << q.pop();
 }
