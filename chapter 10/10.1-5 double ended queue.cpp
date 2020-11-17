717a16352e033cab3916a330e852ddcb128bd0fa
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-11-17 19:18:17 +0900

    chapter 10

--- chapter 10/10.1-5 double ended queue.cpp
+++ chapter 10/10.1-5 double ended queue.cpp
@@ -1,91 +1,49 @@
-#include<iostream>
-#include<array>
-#include<stdexcept>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
 
-template<typename T, size_t N>
-struct Double_ended_queue {
-    std::array<T, N> data;
-    int head = 0;
-    int tail = 0;
-    Double_ended_queue() : data{}, head{ 0 }, tail{ 0 }   {};
+template<typename T>
+class Circular_Deque {
+private:
+    std::vector<T> data;
+    size_t size;
 
-    void head_enqueue(const T x);
-    void tail_enqueue(const T x);
-    T head_dequeue();
-    T tail_dequeue();
+public:
+    Circular_Deque(int k) : 
+        size(k) {}
+
+    void push_front(T value) {
+        assert(data.size() != size);
+        data.insert(data.begin(), value);
+    }
+
+    void push_back(T value) {
+        assert(data.size() != size);
+        data.push_back(value);
+    }
+
+    void pop_front() {
+        assert(!data.empty());
+        data.erase(data.begin());
+    }
+
+    void pop_back() {
+        assert(!data.empty());
+        data.erase(data.begin() + data.size() - 1);
+    }
+
+    T front() {
+        assert(!data.empty());
+        return data.front();
+    }
+
+    T back() {
+        assert(!data.empty());
+        return data.back();
+    }
+
+    bool is_empty() {
+        return data.empty();
+    }
 };
-
-template<typename T, size_t N>
-void Double_ended_queue<T, N>::head_enqueue(const T x)
-{
-    if (head == tail + 1 || (head == 0 && tail == data.size() - 1))
-        throw std::overflow_error("queue overflow");
-    else
-    {
-        if (head == 0)
-            head == data.size() - 1;
-        else
-            head--;
-        data[head] = x;
-    }
-}
-
-template<typename T, size_t N>
-void Double_ended_queue<T, N>::tail_enqueue(const T x)
-{
-    if (head == tail + 1 || (head == 0 && tail == data.size() - 1))
-        throw std::overflow_error("queue overflow");
-    else
-    {
-        data[tail] = x;
-        if (tail == data.size() - 1)
-            tail = 0;
-        else
-            tail++;
-    }
-}
-
-template<typename T, size_t N>
-T Double_ended_queue<T, N>::head_dequeue()
-{
-    // the array is empty
-    if (head == tail)
-        throw std::underflow_error("Queue underflow");
-    else
-    {
-        T x = data[head];
-        if (head == data.size() - 1)
-            head = 0;
-        else
-            head++;
-        return x;
-    }
-}
-
-template<typename T, size_t N>
-T Double_ended_queue<T, N>::tail_dequeue()
-{
-    // the array is empty
-    if (head == tail)
-        throw std::underflow_error("Queue underflow");
-    else
-    {
-        if (tail == 0)
-            tail = data.size() - 1;
-        else
-            tail--;
-        T x = data[tail];
-        return x;
-    }
-}
-
-int main()
-{
-    Double_ended_queue<int, 50> dq;
-
-    dq.tail_enqueue(1);
-    dq.tail_enqueue(2);
-    dq.tail_enqueue(3);
-
-    std::cout << dq.tail_dequeue() << dq.tail_dequeue() << dq.tail_dequeue();
-}
