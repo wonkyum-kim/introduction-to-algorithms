e194af772f3cf2e15ce5747868b9794207417c1d
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-11-24 17:09:03 +0900

    chapter 2

--- chapter 2/insertion sort.cpp
+++ chapter 2/insertion sort.cpp
@@ -1,25 +1,38 @@
-#include <vector>
 #include <iostream>
+#include <algorithm>
+#include <random>
+#include <vector>
+#include <numeric>
 
-template <typename T>
+std::mt19937 gen(std::random_device{}());
+
+template<typename T>
 void insertion_sort(std::vector<T>& A)
 {
-    for (auto j = 1; j < A.size(); ++j) {
-        T key = A[j];
-        auto i = j - 1;
-        while (i>=0 && A[i] > key) {
-            A[i + 1] = A[i];
-            i--;
-        }
-        A[i + 1] = key;
-    }
+	for (auto j = 2; j < A.size(); ++j) {
+		T key = A[j];
+		auto i = j - 1;
+		while (i > 0 && A[i] > key) {
+			A[i + 1] = A[i];
+			i--;
+		}
+		A[i + 1] = key;
+	}
 }
 
 int main()
 {
-    std::vector<int> v{ 5, 2, 3, 1, 4 };
-    insertion_sort(v);
-    for (auto n : v)
-        std::cout << n << ' ';
+	constexpr int SIZE{ 100 };
+	std::vector<int> v(SIZE + 1);
+	std::iota(v.begin() + 1, v.end(), 1);
+	std::shuffle(v.begin() + 1, v.end(), gen);
+	std::cout << "before" << '\n';
+	for (const auto& x : v) {
+		std::cout << x << ' ';
+	}
+	std::cout << '\n' << "after" << '\n';
+	insertion_sort<int>(v);
+	for (const auto& x : v) {
+		std::cout << x << ' ';
+	}
 }
- 
