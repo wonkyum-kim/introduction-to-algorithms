5413644f1975431eb8aec237e9c84dcf391d6b60
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-11-24 17:50:11 +0900

    chapter 2

--- chapter 2/merge sort with sentinel.cpp
+++ chapter 2/merge sort with sentinel.cpp
@@ -1,8 +1,12 @@
-#include <vector>
 #include <iostream>
-#include <limits>
+#include <algorithm>
+#include <random>
+#include <vector>
+#include <numeric>
+#include <utility>
 
-// combine
+std::mt19937 gen(std::random_device{}());
+
 template<typename T>
 void merge(std::vector<T>& A, const size_t p, const size_t q, const size_t r)
 {
@@ -14,27 +18,30 @@ void merge(std::vector<T>& A, const size_t p, const size_t q, const size_t r)
     L.push_back(0);
     R.push_back(0);
 
-    for (auto i = 1; i <= n1; ++i) 
+    for (auto i = 1; i <= n1; ++i) {
         L.push_back(A[p + i - 1]);
-    for (auto j = 1; j <= n2; ++j)
+    }
+    for (auto j = 1; j <= n2; ++j) {
         R.push_back(A[q + j]);
+    }
 
     L.push_back(std::numeric_limits<int>::max());
     R.push_back(std::numeric_limits<int>::max());
-    
+
     size_t i = 1;
     size_t j = 1;
 
     for (auto k = p; k <= r; ++k) {
-        if (L[i] <= R[j])
+        if (L[i] <= R[j]) {
             A[k] = L[i++];
-        else
+        }
+        else {
             A[k] = R[j++];
+        }
     }
 
 }
 
-// devide and conquer
 template<typename T>
 void merge_sort(std::vector<T>& A, const size_t p, const size_t r)
 {
@@ -48,8 +55,12 @@ void merge_sort(std::vector<T>& A, const size_t p, const size_t r)
 
 int main()
 {
-    std::vector<int> A{ 2, 1, 3, 5, 4 };
-    merge_sort(A, 0, A.size() - 1);
-    for (auto& x : A)
-        std::cout << x << ' ';
+	constexpr int SIZE{ 100 };
+	std::vector<int> v(SIZE + 1);
+	std::iota(v.begin() + 1, v.end(), 1);
+	std::shuffle(v.begin() + 1, v.end(), gen);
+    merge_sort<int>(v, 1, v.size() - 1);
+	for (auto x : v) {
+		std::cout << x << ' ';
+	}
 }
