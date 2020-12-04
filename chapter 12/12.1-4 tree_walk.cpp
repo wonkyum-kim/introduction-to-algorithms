c3a820d97e1f6a3bacc407309a7fd10c039cacdb
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-12-05 00:30:37 +0900

    chapter 12

--- chapter 12/12.1-4 tree_walk.cpp
+++ chapter 12/12.1-4 tree_walk.cpp
@@ -72,13 +72,6 @@ public:
 
 	//------------------------------------------------------
 
-	void tree_delete(const T& k) {
-		auto z = tree_search(k);
-		tree_delete(z);
-	}
-
-	//------------------------------------------------------
-
 private:
 
 	//------------------------------------------------------
@@ -170,56 +163,6 @@ private:
 			y = y->parent;
 		}
 		return y;
-	}
-
-	//------------------------------------------------------
-
-	void transplant(Node* u, std::unique_ptr<Node>&& v) {
-		if (v) {
-			v->parent = u->parent;
-		}
-		if (!u->parent) {
-			root = std::move(v);
-		}
-		else if (u == u->parent->left.get()) {
-			u->parent->left = std::move(v);
-		}
-		else {
-			u->parent->right = std::move(v);
-		}
-	}
-
-	void tree_delete(Node* z) {
-		// case 1 : z has one child
-		// case 1-1 : z has a right child
-		if (!z->left) {
-			transplant(z, std::move(z->right));
-		}
-		// case 1-2 : z has a left child
-		else if (!z->right) {
-			transplant(z, std::move(z->left));
-		}
-		// case 2 : z has two children
-		else {
-			Node* y = tree_minimum(z->right.get());
-			std::unique_ptr<Node> upy = nullptr;
-			if (y->parent != z) {
-				// disconnect a link between z->right's subtree and y's subtree
-				upy = std::move(y->parent->left);
-				// disconnect a link between y and y->right's subtree
-				std::unique_ptr<Node> x = std::move(upy->right);
-				// link z->right's subtree with x
-				transplant(tree_minimum(z->right.get())->left.get(), std::move(x));
-				// link y with z->right's subtree
-				transplant(upy->right.get(), std::move(z->right));
-			}
-			else {
-				upy = std::move(z->right);
-			}
-			std::unique_ptr<Node> upzl = std::move(z->left);
-			upy->left = std::move(upzl);
-			transplant(z, std::move(upy));
-		}
 	}
 
 	//------------------------------------------------------
