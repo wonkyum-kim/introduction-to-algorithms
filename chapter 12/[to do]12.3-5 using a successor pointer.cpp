f97809bbbf000481fc06cfbb742df32b4f23ae33
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-12-08 15:56:31 +0900

    chapter 12

--- chapter 12/[to do]12.3-5 using a successor pointer.cpp
+++ chapter 12/[to do]12.3-5 using a successor pointer.cpp
@@ -1,1 +1,250 @@
+#include <iostream>
+#include <utility>
+#include <memory>
+#include <vector>
+#include <numeric>
+#include <algorithm>
+#include <random>
+#include <crtdbg.h>
 
+std::mt19937 gen(std::random_device{}());
+
+template<typename T>
+class BST {
+private:
+	struct Node {
+		T key;
+		Node* succ;
+		std::unique_ptr<Node> left;
+		std::unique_ptr<Node> right;
+		Node(const T& k) :
+			key{ k }, succ{ nullptr }, left{ nullptr }, right{ nullptr } {}
+	};
+
+	std::unique_ptr<Node> root = nullptr;
+
+public:
+	void inorder_tree_walk() {
+		inorder_tree_walk(root.get());
+	}
+
+	void tree_insert(const T& k) {
+		auto z = std::make_unique<Node>(k);
+		Node* y = nullptr;
+		Node* x = root.get();
+		Node* pred = nullptr;
+		while (x) {
+			y = x;
+			if (z->key < x->key) {
+				x = x->left.get();
+			}
+			else {
+				pred = x;
+				x = x->right.get();
+			}
+		}
+		if (!y) {
+			root = std::move(z);
+		}
+		else if (z->key < y->key) {
+			z->succ = y;
+			y->left = std::move(z);
+			if (pred) {
+				pred->succ = y->left.get();
+			}
+		}
+		else {
+			y->right = std::move(z);
+			y->right->succ = y->succ;
+			y->succ = y->right.get();
+		}
+	}
+
+	// check succ pointer
+	void check_succ() {
+		Node* x = tree_search(1);
+		while (x) {
+			std::cout << x->key << ' ';
+			x = x->succ;
+		}
+	}
+
+	// check tree_predecessor function
+	void check_pred() {
+		Node* x = tree_search(100);
+		while (x) {
+			std::cout << x->key << ' ';
+			x = tree_predecessor(x);
+		}
+	}
+
+	Node* tree_search(const T& k) {
+		return tree_search(root.get(), k);
+	}
+
+	void tree_delete(const T& k) {
+		auto z = tree_search(k);
+		if (z) {
+			tree_delete(z);
+		}
+		else {
+			return;
+		}
+	}
+
+private:
+	Node* tree_search(Node* x, const T& k) {
+		if (!x) {
+			return x;
+		}
+		if (k == x->key) {
+			return x;
+		}
+		else if (k < x->key) {
+			return tree_search(x->left.get(), k);
+		}
+		else {
+			return tree_search(x->right.get(), k);
+		}
+	}
+
+	void inorder_tree_walk(Node* x) {
+		if (x) {
+			inorder_tree_walk(x->left.get());
+			std::cout << x->key << ' ';
+			inorder_tree_walk(x->right.get());
+		}
+	}
+
+	Node* parent(Node* x) {
+		if (x == root.get()) {
+			return nullptr;
+		}
+		Node* y = tree_maximum(x)->succ;
+		if (!y) {
+			y = root.get();
+		}
+		else {
+			if (x == y->left.get()) {
+				return y;
+			}
+			y = y->left.get();
+		}
+		while (x != y->right.get()) {
+			y = y->right.get();
+		}
+		return y;
+	}
+
+	Node* tree_maximum(Node* x) {
+		while (x->right) {
+			x = x->right.get();
+		}
+		return x;
+	}
+
+	Node* tree_minimum(Node* x) {
+		while (x->left) {
+			x = x->left.get();
+		}
+		return x;
+	}
+
+	Node* tree_predecessor(Node* x) {
+		if (x->left) {
+			return tree_maximum(x->left.get());
+		}
+		else {	// !x->left
+			Node* y = parent(x);
+			while (y && x == y->left.get()) {
+				x = y;
+				y = parent(y);
+			}
+			return y;
+		}
+	}
+
+	Node* transplant(Node* u, std::unique_ptr<Node>&& v) {
+		Node* up = parent(u);
+		Node* w = nullptr;
+		if (!up) {
+			w = root.release();
+			root = std::move(v);
+		}
+		else if (u == up->left.get()) {
+			w = up->left.release();
+			up->left = std::move(v);
+		}
+		else {		
+			w = up->right.release();
+			up->right = std::move(v);
+		}
+		return w;
+	}
+
+	void tree_delete(Node* z) {
+		Node* pred = tree_predecessor(z);
+		if (pred) {
+			pred->succ = z->succ;
+		}
+		if (!z->left) {
+			Node* pz = transplant(z, std::move(z->right));
+			auto upz = std::unique_ptr<Node>(pz);
+		}
+		else if (!z->right) {
+			Node* pz = transplant(z, std::move(z->left));
+			auto upz = std::unique_ptr<Node>(pz);
+		}
+		else {
+			Node* y = tree_minimum(z->right.get());
+			if (parent(y) == z) {
+				Node* pz = transplant(z, std::move(z->right));
+				y->left = std::move(pz->left);
+				auto upz = std::unique_ptr<Node>(pz);
+			}
+			else {
+				Node* py = transplant(y, std::move(y->right));
+				py->right = std::move(z->right);
+				// delete heap memory
+				auto upy = std::unique_ptr<Node>(py);
+				Node* pz = transplant(z, std::move(upy));
+				py->left = std::move(pz->left);
+				// delete heap memory
+				auto upz = std::unique_ptr<Node>(pz);
+			}
+		}
+	}
+};
+
+int main()
+{
+	BST<int> bst;
+	std::vector<int> v(101);
+	std::iota(v.begin() + 1, v.end(), 1);
+
+	std::shuffle(v.begin() + 1, v.end(), gen);
+	for (auto i = 1; i < v.size(); ++i) {
+		bst.tree_insert(v[i]);
+	}
+
+	bst.inorder_tree_walk();
+	std::cout << '\n';
+	std::cout << '\n';
+
+	std::shuffle(v.begin() + 1, v.end(), gen);
+	for (auto i = 1; i < v.size(); i++) {
+		bst.tree_delete(v[i]);
+	}
+
+	std::cout << "delete complete \n";
+	bst.inorder_tree_walk();
+	std::cout << '\n';
+
+	std::shuffle(v.begin() + 1, v.end(), gen);
+	for (auto i = 1; i < v.size(); ++i) {
+		bst.tree_insert(v[i]);
+	}
+
+	bst.inorder_tree_walk();
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+}
