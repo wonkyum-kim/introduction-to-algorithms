4693bfde7d4d3c12b21e209d4861005bb8cf44d7
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-12-26 18:22:04 +0900

    chapter 22

--- chapter 22/BFS print path.cpp
+++ chapter 22/BFS print path.cpp
@@ -34,10 +34,10 @@ public:
 		}
 	}
 
-	using vertex_u = std::tuple<Color, size_t, size_t>;	// u.color, u.d, u.phi
+	using vertex_info = std::tuple<Color, size_t, size_t>;	// u.color, u.d, u.phi
 
-	std::vector<vertex_u> BFS(size_t s) {
-		std::vector<vertex_u> info(n, { Color::White,std::numeric_limits<size_t>::max(), -1 });
+	std::vector<vertex_info> BFS(size_t s) {
+		std::vector<vertex_info> info(n, { Color::White,std::numeric_limits<size_t>::max(), -1 });
 		info[s] = { Color::Gray, 0, -1 };
 		std::queue<size_t> q;
 		q.push(s);
@@ -46,16 +46,16 @@ public:
 			q.pop();
 			for (auto v : adj[u]) {
 				if (std::get<0>(info[v]) == Color::White) {
-					info[v] = { Color::Gray, std::get<1>(info[v]) + 1, u };
+					info[v] = { Color::Gray, std::get<1>(info[u]) + 1, u };
 					q.push(v);
 				}
 			}
-			std::get<0>(info[u]) = Color::Black;
+			// std::get<0>(info[u]) = Color::Black;
 		}
 		return info;
 	}
 
-	void print_path(size_t s, size_t v, const std::vector<vertex_u>& info) {
+	void print_path(size_t s, size_t v, const std::vector<vertex_info>& info) {
 		if (v == s) {
 			std::cout << s << ' ';
 		}
@@ -69,16 +69,3 @@ public:
 		}
 	}
 };
-
-int main()
-{
-	Graph g(6, Graph_Type::Undirected);
-	g.add_edge(0, 3);
-	g.add_edge(0, 1);
-	g.add_edge(1, 4);
-	g.add_edge(1, 2);
-	g.add_edge(2, 5);
-	g.add_edge(4, 5);
-	auto info = g.BFS(0);
-	g.print_path(0, 2 ,info);
-}
