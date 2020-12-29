71cbab78939f1370785691ea79563ef599831b18
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2020-12-29 17:29:37 +0900

    chapter 22

---
+++ chapter 22/topological sort.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <tuple>
+#include <numeric>
+#include <stack>
+#include <utility>
+#include <list>
+#include <cassert>
+
+enum class Graph_Type {
+	Directed, Undirected
+};
+
+class Graph {
+private:
+	using vertex = size_t;
+	using edge_weight = double;
+	using edge = std::tuple<vertex, vertex, edge_weight>;
+
+	enum class Color {
+		White, Gray, Black
+	};
+
+	vertex n;
+	Graph_Type type;
+	std::vector<std::vector<size_t>> adj;
+	std::list<size_t> sorted;
+
+public:
+	Graph(size_t n, Graph_Type dir) : n{ n }, type{ dir }, adj(n) {}
+
+	void add_edge(size_t u, size_t v) {
+		adj[u].push_back(v);
+		if (type == Graph_Type::Undirected) {
+			adj[v].push_back(u);
+		}
+	}
+
+	void sort_adj() {
+		for (auto i = 0; i < n; ++i) {
+			std::sort(adj[i].begin(), adj[i].end());
+		}
+	}
+
+	using vertex_info = std::tuple<Color, size_t, size_t, size_t>;	// u.color, u.d, u.f, u.phi
+
+	void DFS() {
+		std::vector<vertex_info> info(n, { Color::White, 0, 0, -1 });
+		size_t time = 0;
+		for (auto i = 0; i < n; ++i) {
+			if (std::get<0>(info[i]) == Color::White) {
+				DFS_visit(i, time, info);
+			}
+		}
+	}
+
+	std::list<size_t> topological_sort() {
+		assert(type == Graph_Type::Directed);
+		DFS();
+		return sorted;
+	}
+
+private:
+	void DFS_visit(size_t u, size_t& time, std::vector<vertex_info>& info) {
+		++time;
+		std::get<1>(info[u]) = time;
+		std::get<0>(info[u]) = Color::Gray;
+		for (auto v : adj[u]) {
+			if (std::get<0>(info[v]) == Color::White) {
+				std::get<3>(info[v]) = u;
+				DFS_visit(v, time, info);
+			}
+		}
+		std::get<0>(info[u]) = Color::Black;
+		++time;
+		std::get<2>(info[u]) = time;
+		sorted.push_front(u);
+	}
+};
+
+int main()
+{
+	Graph g(6, Graph_Type::Directed);
+	g.add_edge(0, 1);
+	g.add_edge(1, 2);
+	g.add_edge(2, 5);
+	g.add_edge(3, 0);
+	g.add_edge(3, 4);
+	g.add_edge(4, 1);
+	g.add_edge(4, 2);
+	g.sort_adj();
+	auto topological_sort = g.topological_sort();
+	while (!topological_sort.empty()) {
+		size_t node = topological_sort.front();
+		topological_sort.pop_front();
+		std::cout << node << ' ';
+	}
+}
