005af0a2169e1583b75bcec111af751c558bc3b2
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2021-01-01 22:19:36 +0900

    chapter 22

--- chapter 22/DFS.cpp
+++ chapter 22/DFS.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <tuple>
-#include <numeric>
-#include <queue>
-#include <utility>
 
 enum class Graph_Type {
 	Directed, Undirected
@@ -19,50 +15,55 @@ private:
 		White, Gray, Black
 	};
 
+	struct vertex_info {
+		Color color = Color::White;
+		size_t d = 0;
+		size_t f = 0;
+		size_t phi = -1;
+	};
+
 	vertex n;
-	Graph_Type type;
-	std::vector<std::vector<size_t>> adj;
+	Graph_Type g_type;
+	std::vector<std::vector<vertex>> adj;
+	std::vector<vertex_info> info;
 
 public:
-	Graph(size_t n, Graph_Type dir) : n{ n }, type{ dir }, adj(n) {}
+	Graph(vertex n, Graph_Type g_type) : n{n},g_type{g_type},adj(n), info(n){}
 
-	void add_edge(size_t u, size_t v) {
+	void add_edge(vertex u, vertex v) {
 		adj[u].push_back(v);
-		if (type == Graph_Type::Undirected) {
+		if (g_type == Graph_Type::Undirected) {
 			adj[v].push_back(u);
 		}
 	}
 
-	using vertex_info = std::tuple<Color, size_t, size_t, size_t>;	// u.color, u.d, u.f, u.phi
-
 	void DFS() {
-		std::vector<vertex_info> info(n, { Color::White, 0, 0, -1 });
 		size_t time = 0;
 		for (auto i = 0; i < n; ++i) {
-			if (std::get<0>(info[i]) == Color::White) {
-				DFS_visit(i, time, info);
+			if (info[i].color == Color::White) {
+				DFS_visit(i, time);
 			}
 		}
 	}
 
 private:
-	void DFS_visit(size_t u, size_t& time, std::vector<vertex_info>& info) {
+	void DFS_visit(vertex u, size_t& time) {
 		++time;
-		std::get<1>(info[u]) = time;
-		std::get<0>(info[u]) = Color::Gray;
+		info[u].d = time;
+		info[u].color = Color::Gray;
 		for (auto v : adj[u]) {
-			if (std::get<0>(info[v]) == Color::White) {
-				std::get<3>(info[v]) = u;
-				DFS_visit(v, time, info);
+			if (info[v].color == Color::White) {
+				info[v].phi = u;
+				DFS_visit(v, time);
 			}
 		}
-		std::get<0>(info[u]) = Color::Black;
+		info[u].color = Color::Black;
 		++time;
-		std::get<2>(info[u]) = time;
+		info[u].f = time;
 	}
 };
 
-int main()
+int main() 
 {
 	Graph g(6, Graph_Type::Undirected);
 	g.add_edge(0, 3);
@@ -71,5 +72,5 @@ int main()
 	g.add_edge(1, 2);
 	g.add_edge(2, 5);
 	g.add_edge(4, 5);
-	g.DFS();	
+	g.DFS();
 }
