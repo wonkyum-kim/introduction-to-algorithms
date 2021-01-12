be76db247d40b7a81a712d9255f9d2caf18bf951
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2021-01-13 04:36:32 +0900

    chapter 15

--- chapter 15/problem 15-3 bitonic_tour.cpp
+++ chapter 15/problem 15-3 bitonic_tour.cpp
@@ -1,46 +1,55 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
 #include <vector>
 #include <utility>
 #include <numeric>
+#include <iomanip>
 
-using Point = std::pair<double, double>;
+using Point = std::pair<long double, long double>;
 
-double distance(const Point& p1, const Point& p2)
+long double distance(const Point& p1, const Point& p2)
 {
 	return std::hypot(p1.first - p2.first, p1.second - p2.second);
 }
 
-double bitonic_tour(std::vector<Point>& p) 
+long double bitonic_tour(std::vector<Point>& p)
 {
-	std::sort(p.begin(), p.end());
-	const size_t n = p.size();
-	std::vector<std::vector<double>> dp(n, std::vector<double>(n, -1));
-	dp[0][0] = 0;
-	for (auto i = 1; i < n; ++i) {
-		dp[0][i] = dp[0][i - 1] + distance(p[i - 1], p[i]);
-	}
-	for (auto i = 1; i < n; ++i) {
-		for (auto j = i; j < n; ++j) {
-			auto minimum = std::numeric_limits<double>::max();
-			if (i == j || i == j - 1) {
-				for (auto k = 0; k < i; ++k) {
-					auto opt = dp[k][i] + distance(p[k], p[j]);
-					minimum = std::min(minimum, opt);
-				}
-				dp[i][j] = minimum;
-			}
-			else {
-				dp[i][j] = dp[i][j - 1] + distance(p[j - 1], p[j]);
-			}
+	std::sort(p.begin() + 1, p.end());
+	size_t n = p.size() - 1;
+	// dp[a][b] (a < b)
+	std::vector<std::vector<long double>> dp(n + 1, std::vector<long double>(n + 1));
+	dp[1][2] = distance(p[1], p[2]);
+	for (auto j = 3; j < n + 1; ++j) {
+		// k > 1
+		// route 1 : from p[1] to p[i]
+		// route 2 : from p[1] to p[i + k]
+		// dp[i][i + k] = dp[i][i + k - 1] + distance(p[i + k - 1], p[i + k])
+		for (auto i = 1; i < j - 1; ++i) {
+			dp[i][j] = dp[i][j - 1] + distance(p[j - 1], p[j]);
+		}
+		// k = 1
+		// route 1 : from p[1] to p[i]
+		// route 2 : from p[1] to p[i + 1]
+		// dp[i][i + 1] = min(dp[i][i + 1], dp[0 .. i - 1][i] + distnace(p[0 .. i - 1] + p[i + 1])
+		dp[j - 1][j] = std::numeric_limits<long double>::max();
+		for (auto i = 1; i < j - 1; ++i) {
+			dp[j - 1][j] = std::min(dp[j - 1][j], dp[i][j - 1] + distance(p[i], p[j]));
 		}
 	}
-	return dp[n - 1][n - 1];
-}
 
+	long double ans = std::numeric_limits<long double>::max();
+	for (auto i = 1; i < n; ++i) {
+		ans = std::min(ans, dp[i][n] + distance(p[i], p[n]));
+	}
+	return ans;
+}
 
 
 int main()
 {
 	std::vector<Point> p;
+	p.push_back({ 0.0, 0.0 });	// dummy
 	p.push_back({ 0.0, 0.0 });
 	p.push_back({ 1.0, 6.0 });
 	p.push_back({ 2.0, 3.0 });
