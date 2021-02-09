c39feea30d30664075cde31df002c91c3b3ecaf7
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2021-02-09 20:25:59 +0900

    chapter 24

---
+++ chapter 24/24.1-6 vertices of a negative cycle.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <cmath>
+#include <tuple>
+#include <numeric>
+
+constexpr auto INF = std::numeric_limits<long long>::max();
+
+int V = 0;
+int E = 0;
+std::vector<std::vector<std::pair<int, int>>> adj;
+std::vector<long long> distance;
+std::vector<int> parent;
+std::vector<int> negative_cycle;
+
+void initialize_sigle_source(int s)
+{
+	distance.resize(V, INF);
+	parent.resize(V, -1);
+	distance[s] = 0;
+}
+
+bool Bellman_Ford(int s)
+{
+	initialize_sigle_source(s);
+	for (auto i = 1; i <= V - 1; ++i) {
+		bool flag = false;
+		for (auto a = 0; a < V; ++a) {
+			for (auto& [b, w] : adj[a]) {
+				// Relax
+				if (distance[a] != INF && distance[b] > distance[a] + w) {
+					distance[b] = distance[a] + w;
+					parent[b] = a;
+					flag = true;
+				}
+			}
+		}
+		if (flag) {
+			break;
+		}
+	}
+
+	for (auto a = 0; a < V; ++a) {
+		for (auto& [b, w] : adj[a]) {
+			if (distance[a] != INF && distance[b] > distance[a] + w) {
+				int curr = b;
+				do {
+					negative_cycle.push_back(curr);
+					curr = parent[curr];
+				} while (curr != b);
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	std::cin >> V >> E;
+	adj.resize(V);
+	for (auto i = 0; i < E; ++i) {
+		int a = 0;
+		int b = 0;
+		int w = 0;
+		std::cin >> a >> b >> w;
+		adj[a].push_back({ b,w });
+	}
+
+	bool flag = Bellman_Ford(0);
+
+	if (!flag) {
+		for (auto x : negative_cycle) {
+			std::cout << x << ' ';
+		}
+	}
+
+}
+
+/*
+	4 4
+	0 1 2
+	1 2 -2
+	2 1 -2
+	2 3 1
+*/
