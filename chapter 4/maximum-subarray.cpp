19ecc18fcf42f0ab8ae909fc1a244baf524f7087
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2021-03-09 22:40:30 +0900

    chapter 4

--- chapter 4/maximum-subarray.cpp
+++ chapter 4/maximum-subarray.cpp
@@ -3,16 +3,15 @@
 #include <limits>
 #include <tuple>
 
-using ans_type = std::tuple<size_t, size_t, int>;
+using tuple = std::tuple<int, int, int>;
 
-template<typename T>
-ans_type find_max_crossing_subarray(const std::vector<T>& A, const size_t low, const size_t mid, const size_t high)
+tuple find_max_crossing_subarray(const std::vector<int>& A, const int low, const int mid, const int high)
 {
-	int left_sum = std::numeric_limits<int>::min();		// sentinel value
+	int left_sum = std::numeric_limits<int>::min();		
 	int sum = 0;
-	size_t max_left = 0;
+	int max_left = 0;
 
-	for (size_t i = mid; i >= low; --i) {
+	for (int i = mid; i >= low; --i) {
 		sum += A[i];
 		if (sum > left_sum) {
 			left_sum = sum;
@@ -20,11 +19,11 @@ ans_type find_max_crossing_subarray(const std::vector<T>& A, const size_t low, c
 		}
 	}
 
-	int right_sum = std::numeric_limits<int>::min();	// sentinel value
+	int right_sum = std::numeric_limits<int>::min();	
 	sum = 0;
-	size_t max_right = 0;
+	int max_right = 0;
 
-	for (size_t i = mid + 1; i <= high; ++i) {
+	for (int i = mid + 1; i <= high; ++i) {
 		sum += A[i];
 		if (sum > right_sum) {
 			right_sum = sum;
@@ -34,31 +33,33 @@ ans_type find_max_crossing_subarray(const std::vector<T>& A, const size_t low, c
 	return { max_left, max_right, left_sum + right_sum };
 }
 
-template <typename T>
-ans_type find_maximum_subarray(const std::vector<T>& A, const size_t low, const size_t high)
+tuple find_maximum_subarray(const std::vector<int>& A, const int low, const int high)
 {
 	if (high == low)	// base case: only one element
 		return { low, high, A[low] };
 	else {
-		size_t mid = (low + high) / 2;
-		auto [l_low, l_high, l_sum] = find_maximum_subarray(A, low, mid);
-		auto [r_low, r_high, r_sum] = find_maximum_subarray(A, mid + 1, high);
-		auto [c_low, c_high, c_sum] = find_max_crossing_subarray(A, low, mid, high);
+		int mid = (low + high) / 2;
+		auto [left_low, left_high, left_sum] = find_maximum_subarray(A, low, mid);
+		auto [right_low, right_high, right_sum] = find_maximum_subarray(A, mid + 1, high);
+		auto [cross_low, cross_high, cross_sum] = find_max_crossing_subarray(A, low, mid, high);
 
-		if (l_sum >= r_sum && l_sum >= c_sum)
-			return { l_low, l_high, l_sum };
-		else if (r_sum >= l_sum && r_sum >= c_sum)
-			return { r_low, r_high, r_sum };
-		else
-			return { c_low, c_high, c_sum };
+		if (left_sum >= right_sum && left_sum >= cross_sum) {
+			return { left_low, left_high, left_sum };
+		}
+		else if (right_sum >= left_sum && right_sum >= cross_sum) {
+			return { right_low, right_high, right_sum };
+		}
+		else {
+			return { cross_low, cross_high, cross_sum };
+		}
 	}
 }
 
 int main()
 {
 	std::vector<int> A{ 0, 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
-	size_t low = 1;
-	size_t high = 16;
+	int low = 1;
+	int high = 16;
 	auto arr = find_maximum_subarray(A, low, high);
 	std::cout << std::get<0>(arr) << ' ' << std::get<1>(arr) << ' ' << std::get<2>(arr);
-}	
+}
