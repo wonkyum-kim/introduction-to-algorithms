663553f66907f3a332011309a615adc886e08f47
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2021-03-09 23:32:25 +0900

    chapter 15

--- chapter 15/extended_bottom_up_cut_rod.cpp
+++ chapter 15/extended_bottom_up_cut_rod.cpp
@@ -6,43 +6,36 @@
 
 // bottom up
 
-template<typename T>
-std::pair<std::vector<T>, std::vector<T>> extended_bottom_up_cut_rod(const std::vector<T>& p, int n)
+using pair = std::pair<std::vector<int>, std::vector<int>>;
+
+pair extended_bottom_up_cut_rod(const std::vector<int>& p, int n)
 {
-	// r is a max_value array
-	// s is a devided length array
-	std::vector<T> r(n + 1);
-	std::vector<T> s(n + 1);
-	for (auto j = 1; j <= n; ++j) {
-		int q = std::numeric_limits<int>::min();
-		for (auto i = 1; i <= j; ++i) {
-			if (q < p[i] + r[j - i]) {
-				q = p[i] + r[j - i];
-				s[j] = i;
+	std::vector<int> dp(n + 1);
+	std::vector<int> length(n + 1);
+	for (auto i = 1; i <= n; ++i) {
+		for (auto j = 1; j <= i; ++j) {
+			if (dp[i] < dp[i - j] + p[j]) {
+				dp[i] = dp[i - j] + p[j];
+				length[i] = j;
 			}
 		}
-		r[j] = q;
 	}
-	return { r, s };
+	return { dp, length };
 }
 
-void print_cut_rod_solution(const std::vector<int>& p, int n)
+void print_cur_rod_solution(std::vector<int>& p, int n)
 {
-	auto [a, b] = extended_bottom_up_cut_rod(p, n);
+	auto [dp, length] = extended_bottom_up_cut_rod(p, n);
 	while (n > 0) {
-		std::cout << b[n] << ' ';
-		n -= b[n];
+		std::cout << length[n] << ' ';
+		n -= length[n];
 	}
 }
 
 int main()
 {
-	constexpr int dummy = 0;
 	int n = 0;
 	std::cin >> n;
-	std::vector<int> p{ dummy,1,5,8,9,10,17,17,20,24,30 };
-	
-	auto [a, b] = extended_bottom_up_cut_rod(p, n);
-	std::cout << a[n] << '\n';
-	print_cut_rod_solution(p, n);
+	std::vector<int> p{0,1,5,8,9,10,17,17,20,24,30 };
+	print_cur_rod_solution(p, n);
 }
