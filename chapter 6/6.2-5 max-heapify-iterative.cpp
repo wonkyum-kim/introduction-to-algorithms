8e918dcf6ba4257bf28b1559272550b2cb2892a4
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2021-11-08 11:10:14 +0900

    Update 6.2-5 max-heapify-iterative.cpp

--- chapter 6/6.2-5 max-heapify-iterative.cpp
+++ chapter 6/6.2-5 max-heapify-iterative.cpp
@@ -1,65 +1,75 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<cassert>
-#include<random>
-#include<numeric>
+#include <algorithm>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <iomanip>
+#include <numeric>
+#include <string>
+#include <queue>
+#include <cassert>
+#include <map>
+#include <set>
+#include <tuple>
+#include <deque>
+#include <stack>
+#include <random>
 
 std::mt19937 gen(std::random_device{}());
+std::vector<int> A;
 
-// the index of the root node is 1.
-size_t parent(size_t i)
+int parent(int i)
 {
-    return i / 2;
+	return i >> 1;
 }
 
-size_t left(size_t i)
+int left(int i)
 {
-    return 2 * i;
+	return i << 1;
 }
 
-size_t right(size_t i)
+int right(int i)
 {
-    return 2 * i + 1;
+	return (i << 1) + 1;
 }
 
-template <typename T>
-void max_heapify(std::vector<T>& A, size_t h_size, size_t i) {
-    while (i <= h_size / 2) {
-        size_t l = left(i);
-        size_t r = right(i);
-        size_t largest = i;
-        if (l <= h_size && A[l] > A[i]) {
-            largest = l;
-        }
-        if (r <= h_size && A[r] > A[largest]) {
-            largest = r;
-        }
-        if (largest != i) {
-            std::swap(A[i], A[largest]);
-            i = largest;
-        }
-        else {
-            return;
-        }
-    }
+void max_heapify(int i)
+{
+	while (i < A.size()) {
+		int l = left(i);
+		int r = right(i);
+		int largest = i;
+		if (l < A.size() && A[l] > A[i]) {
+			largest = l;
+		}
+		if (r < A.size() && A[r] > A[largest]) {
+			largest = r;
+		}
+		if (largest != i) {
+			std::swap(A[i], A[largest]);
+			i = largest;
+		}
+		else {
+			return;
+		}
+	}
 }
 
-template<typename T>
-void build_max_heap(std::vector<T>& A)
+void build_max_heap()
 {
-    size_t h_size = A.size() - 1;
-    for (size_t i = h_size / 2; i > 0; --i)
-        max_heapify(A, h_size, i);
+	for (int i = (A.size() - 1) / 2; i >= 1; --i) {
+		max_heapify(i);
+	}
 }
 
 int main()
 {
-    std::vector<int> v(11);
-    std::iota(v.begin() + 1, v.end(), 1);
-    std::shuffle(v.begin() + 1, v.end(), gen);
-    build_max_heap(v);
-    for (auto i = 1; i <= 10; ++i) {
-        std::cout << v[i] << ' ';
-    }
+	A.resize(10);
+	std::iota(A.begin() + 1, A.end(), 1);
+	std::shuffle(A.begin() + 1, A.end(), gen);
+	build_max_heap();
+	for (auto x : A) {
+		std::cout << x << ' ';
+	}
+
 }
