c19ba10e662b1f5345728b2d1ef1e6a884356197
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2022-07-20 22:55:45 +0900

    chapter 2

--- chapter 2/2.2-2 selection sort.cpp
+++ chapter 2/2.2-2 selection sort.cpp
@@ -8,27 +8,26 @@
 std::mt19937 gen(std::random_device{}());
 
 template<typename T>
-void selection_sort(std::vector<T>& A)
-{
-	for (auto i = 1; i < A.size() - 1; ++i) {
-		auto min_index = i;
-		for (auto j = i + 1; j < A.size(); ++j) {
-			if (A[min_index] > A[j]) {
-				min_index = j;
-			}
-		}
-		std::swap(A[i], A[min_index]);
-	}
+void selection_sort(std::vector<T>& a) {
+    for (int i = 0; i < a.size() - 1; ++i) {
+        int min_index = i;
+        for (int j = i + 1; j < a.size(); ++j) {
+            if (a[min_index] > a[j]) {
+                min_index = j;
+            }
+        }
+        std::swap(a[i], a[min_index]);
+    }
 }
 
-int main()
+int main() 
 {
-	constexpr int SIZE{ 100 };
-	std::vector<int> v(SIZE + 1);
-	std::iota(v.begin() + 1, v.end(), 1);
-	std::shuffle(v.begin() + 1, v.end(), gen);
-	selection_sort<int>(v);
-	for (auto x : v) {
-		std::cout << x << ' ';
-	}
+    std::vector<int> a(100);
+    std::iota(a.begin(), a.end(), 1);
+    std::shuffle(a.begin(), a.end(), gen);
+    selection_sort<int>(a);
+    for (auto x : a) {
+        std::cout << x << ' ';
+    }
+    std::cout << '\n';
 }
