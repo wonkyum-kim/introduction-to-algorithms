ec341b093dff3a6ed7745239ce6055092630b007
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2023-06-11 21:32:58 +0900

    edit problem 2-2

--- chapter 2/problem 2-2 bubble sort.cpp
+++ chapter 2/problem 2-2 bubble sort.cpp
@@ -1,23 +1,22 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
-template<typename T>
-void bubble_sort(std::vector<T>& A)
+void bubble_sort(std::vector<int>& A)
 {
-	for (size_t i = 0; i < A.size() - 1; ++i) {
-		for (size_t j = A.size() - 1; j > i; --j) {
-			if (A[j] < A[j - 1]) {
-				std::swap(A[j], A[j - 1]);
-			}
-		}
-	}
+    for (int i = 0; i < A.size() - 1; ++i) {
+        for (int j = A.size() - 1; j > i; --j) {
+            if (A[j] < A[j - 1]) {
+                std::swap(A[j], A[j - 1]);
+            }
+        }
+    }
 }
 
 int main()
 {
-	std::vector<int> A{ 5,2,4,1,3 };
-	bubble_sort(A);
-	for (auto& x : A)
-		std::cout << x << ' ';
+    std::vector<int> A { 5, 2, 4, 1, 3 };
+    bubble_sort(A);
+    for (auto& x : A)
+        std::cout << x << ' ';
 }
