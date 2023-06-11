bec80df9ef311b0a4dd45b848d6cbde5b05a5fa1
wonkyum-kim <67191801+wonkyum-kim@users.noreply.github.com>
2023-06-11 21:42:41 +0900

    edit problem 2-4

--- chapter 2/problem 2-4 inversions.cpp
+++ chapter 2/problem 2-4 inversions.cpp
@@ -1,52 +1,53 @@
-#include <iostream>
 #include <algorithm>
-#include <vector>
+#include <iostream>
 #include <numeric>
 #include <utility>
+#include <vector>
 
-size_t merge(std::vector<int>& A, size_t p, size_t q, size_t r)
+int merge(std::vector<int>& A, int p, int q, int r)
 {
-	size_t n1 = q - p + 1;
-	size_t n2 = r - q;
-	std::vector<int> L(n1 + 1);
-	std::vector<int> R(n2 + 1);
-	for (auto i = 1; i <= n1; ++i) {
-		L[i] = A[p + i - 1];
-	}
-	for (auto j = 1; j <= n2; ++j) {
-		R[j] = A[q + j];
-	}
-	L.push_back(std::numeric_limits<int>::max());
-	R.push_back(std::numeric_limits<int>::max());
-	size_t i = 1;
-	size_t j = 1;
-	size_t inversions = 0;
-	for (auto k = p; k <= r; ++k) {
-		if (L[i] <= R[j]) {
-			A[k] = L[i++];
-		}
-		else {
-			inversions += n1 - i + 1;
-			A[k] = R[j++];
-		}
-	}
-	return inversions;
+    int n1 = q - p + 1;
+    int n2 = r - q;
+
+    std::vector<int> L;
+    std::vector<int> R;
+
+    for (int i = 0; i < n1; ++i) {
+        L.push_back(A[p + i]);
+    }
+    for (int j = 0; j < n2; ++j) {
+        R.push_back(A[q + j + 1]);
+    }
+
+    L.push_back(std::numeric_limits<int>::max());
+    R.push_back(std::numeric_limits<int>::max());
+
+    int i = 0;
+    int j = 0;
+    int inversions = 0;
+
+    for (int k = p; k <= r; ++k) {
+        if (L[i] <= R[j]) {
+            A[k] = L[i++];
+        } else {
+            inversions += n1 - i;
+            A[k] = R[j++];
+        }
+    }
+    return inversions;
 }
 
-size_t count_inversions(std::vector<int>& A, size_t p, size_t r)
+int count_inversions(std::vector<int>& A, int p, int r)
 {
-	if (p < r) {
-		size_t q = (p + r) / 2;
-		size_t left = count_inversions(A, p, q);
-		size_t right = count_inversions(A, q + 1, r);
-		size_t inversions = left + right + merge(A, p, q, r);
-		return inversions;
-	}
-	return 0;
+    if (p < r) {
+        int q = (p + r) / 2;
+        return count_inversions(A, p, q) + count_inversions(A, q + 1, r) + merge(A, p, q, r);
+    }
+    return 0;
 }
 
 int main()
 {
-	std::vector<int> v{ 0,2,3,8,6,1 };
-	std::cout << count_inversions(v, 1, 5);
+    std::vector<int> v { 4, 2, 7, 1, 5, 6, 3 };
+    std::cout << count_inversions(v, 0, v.size() - 1);
 }
